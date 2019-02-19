beb89e1f1d0e38b02c7d6b3268894e28ae713d85
Karol Chrapek <kaarol@it-flow.pl>
2019-02-19 13:32:54 +0100

    Update test

--- tests/test_default.py
+++ tests/test_default.py
@@ -3,10 +3,9 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-@pytest.mark.parametrize("file, content', [
+@pytest.mark.parametrize('file, content', [
   ("root/.bashrc", "alias ls='ls  --color=auto'"),
   ("/etc/skel/.bashrc", "HISTSIZE=1000")
-
 ])
 def test_files(host, file, content):
     file = host.file(file)
